8961099a91ac5ac97a075d8849234c3e4c96ed28
Jason <jlee55504@gmail.com>
2025-02-27 22:50:49 -0800

    Added a 'Footer' component displaying the a copyright symbol, the current year, and my name.

---
+++ src/Layout/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { MDBContainer } from 'mdb-react-ui-kit';
+
+function Footer() {
+    return (
+        <footer class="jumbotron end-footer mb-0 bg-dark">
+          <MDBContainer className="text-white"><p className="text-center mb-0 footer-p">&copy; 2025 Jason Lee</p></MDBContainer>
+        </footer>
+    );
+};
+
+export default Footer;
